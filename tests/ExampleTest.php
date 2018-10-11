e4ed8c559c5e209ab209eae30c1ac081eb9f3794
Sernin van de Krol <serninpc@paneidos.net>
2018-10-12 00:54:40 +0200

    Add test setup

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Paneidos\LaravelCrontab;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase {
+    public function testExample() {
+        $this->assertTrue(true);
+    }
+}
